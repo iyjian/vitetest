e18cdce1ae7df7db3892d3aeb2b468832ca96fe3
w.c <52445078@qq.com>
2022-07-26 12:45:46 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+<span>fuck</span>
+</template>
+<script>
+import * as Minio from 'minio'
+// import fxp from "fast-xml-parser"
+// import * as Minio from '/Users/wuchong/projects/forked/minio-js/dist/main/minio.js'
+// const Minio = require('/Users/wuchong/projects/forked/minio-js/dist/main/minio.js')
+
+export default {
+  name: 'Login',
+  mounted () {
+    const c = new Minio.Client({
+      accessKey: 'RhRkAQYN5cDsdvZK',
+      secretKey: 'TEYZqZDMLd3zIWIIun7IDhy7FtKapuhw',
+      endPoint: '0.0.0.0',
+      port: 9000,
+      useSSL: false
+    })
+    c.listBuckets().then(console.log)
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// import {Buffer} from 'buffer';
+// import EventEmitter from 'events';
+// import process from 'process';
+
+// window.Buffer = Buffer;
+// window.EventEmitter = EventEmitter;
+// window.process = process;
+
+
+createApp(App).mount('#app')
